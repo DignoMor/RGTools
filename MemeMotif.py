edec7c0117c74782d60a10d6e0c9f58af72fbd82
Xiuqi Pan <xp76@cornell.edu>
2025-04-23 15:50:39 -0400

    Add MemeMotif implementation

--- MemeMotif.py
+++ MemeMotif.py
@@ -1,4 +1,7 @@
 
+import re 
+import numpy as np
+
 class MemeMotif:
     def __init__(self, file_path: str):
         '''
@@ -7,7 +10,16 @@ class MemeMotif:
         Keyword arguments:
         - file_path: Path to the MEME motif file.
         '''
-        pass
+        self.file_path = file_path
+        self.version = None
+        self.alphabet = None
+        self.strands = None
+        self.bg_freq = None
+        self.motifs = []
+        self.motif_info_dict = {}
+
+        self._parse_meme_file(file_path)
+        
 
     def _parse_meme_file(self, file_path: str):
         '''
@@ -17,7 +29,67 @@ class MemeMotif:
         Keyword arguments:
         - file_path: Path to the MEME motif file.
         '''
-        pass
+
+        with open(file_path, "r") as f:
+            while line := f.readline():
+                line = line.strip()
+                if line.startswith("MEME version"):
+                    self.version = line.split()[-1].strip()
+                    break
+            
+            while line := f.readline():
+                line = line.strip()
+                if line.startswith("ALPHABET"):
+                    self.alphabet = line.split("=")[1].strip()
+                    break
+
+            while line := f.readline():
+                line = line.strip()
+                if line.startswith("strands"):
+                    self.strands = line.split()[1:]
+                    break
+
+            while line := f.readline():
+                line = line.strip()
+                if line.startswith("Background letter frequencies"):
+                    line = f.readline().strip()
+                    line_info = line.split()
+
+                    freq_dict = {}
+
+                    for i in range(len(self.alphabet)):
+                        char = line_info[2*i]
+                        freq = line_info[2*i + 1]
+
+                        freq_dict[char] = freq
+                    
+                    self.bg_freq = [float(freq_dict[c]) for c in self.alphabet]
+                    break
+
+            while line := f.readline():
+                line = line.strip()
+                if line.startswith("MOTIF"):
+                    motif_name = line.split()[1]
+                    motif_info = {}
+
+                    line = f.readline().strip()
+                    match = re.match(r"letter-probability matrix: alength=(.*)w=(.*)nsites=(.*)E=(.*)", line)
+
+                    motif_info["alphabet_length"] = int(match.group(1))
+                    motif_info["motif_length"] = int(match.group(2))
+                    motif_info["num_source_sites"] = int(match.group(3))
+                    motif_info["source_eval"] = float(match.group(4))
+
+                    pwm_arr = np.zeros((motif_info["motif_length"], motif_info["alphabet_length"]))
+                    for i in range(motif_info["motif_length"]):
+                        line = f.readline().strip()
+                        line_info = line.split()
+                        pwm_arr[i] = np.array(line_info, dtype=float)
+                
+                    motif_info["pwm"] = pwm_arr
+
+                    self.motifs.append(motif_name)
+                    self.motif_info_dict[motif_name] = motif_info
 
     def get_meme_version(self):
         '''
@@ -26,7 +98,7 @@ class MemeMotif:
         Return:
         - A string representing the version of the MEME motif file.
         '''
-        pass
+        return self.version
 
     def get_alphabet(self):
         '''
@@ -35,7 +107,7 @@ class MemeMotif:
         Return: 
         - A string representing the alphabet used in the MEME motif file.
         '''
-        pass
+        return self.alphabet
 
     def get_strands(self):
         '''
@@ -44,7 +116,7 @@ class MemeMotif:
         Return:
         - A list of strings representing the strands used in the MEME motif file.
         '''
-        pass
+        return self.strands
 
     def get_bg_freq(self):
         '''
@@ -53,7 +125,7 @@ class MemeMotif:
         Return: 
         - A dictionary with keys being the alphabet characters and values being their frequencies.
         '''
-        pass
+        return self.bg_freq
 
     def get_motif_list(self):
         '''
@@ -62,7 +134,7 @@ class MemeMotif:
         Return: 
         - A list of strings representing the names of the motifs in the MEME motif file.
         '''
-        pass
+        return self.motifs
 
     def get_motif_pwm(self, motif_name):
         '''
@@ -75,7 +147,7 @@ class MemeMotif:
         - A 2D numpy array representing the PWM of the given motif.
           The array should have shape (num_positions, num_alphabet_chars).
         '''
-        pass
+        return self.motif_info_dict[motif_name]["pwm"]
 
     def get_motif_alphabet_length(self, motif_name):
         '''
@@ -87,7 +159,7 @@ class MemeMotif:
         Return: 
         - An integer representing the length of the alphabet used in the given motif.
         '''
-        pass
+        return self.motif_info_dict[motif_name]["alphabet_length"]
 
     def get_motif_length(self, motif_name):
         '''
@@ -99,7 +171,7 @@ class MemeMotif:
         Return:
         - An integer representing the length of the given motif.
         '''
-        pass
+        return self.motif_info_dict[motif_name]["motif_length"]
 
     def get_motif_num_source_sites(self, motif_name):
         '''
@@ -111,7 +183,7 @@ class MemeMotif:
         Return: 
         - An integer representing the number of source sites for the given motif.
         '''
-        pass
+        return self.motif_info_dict[motif_name]["num_source_sites"] 
 
     def get_motif_source_eval(self, motif_name):
         '''
@@ -123,6 +195,6 @@ class MemeMotif:
         Return: 
         - A float representing the source E-value for the given motif.
         '''
-        pass
+        return self.motif_info_dict[motif_name]["source_eval"]
     
 
