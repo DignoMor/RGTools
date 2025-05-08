fc99b778c600af0529ea6f5d5ed909a252dac050
Xiuqi Pan <xp76@cornell.edu>
2025-05-08 14:45:03 -0400

    Add downloading large files script for testing

---
+++ download_large_files.sh
@@ -0,0 +1,17 @@
+
+wget \
+    -O large_files/ENCFF775FNU.mn.bw \
+    https://www.encodeproject.org/files/ENCFF775FNU/@@download/ENCFF775FNU.bigWig
+
+wget \
+    -O large_files/ENCFF565BWR.pl.bw \
+    https://www.encodeproject.org/files/ENCFF565BWR/@@download/ENCFF565BWR.bigWig
+
+wget \
+    -O large_files/hg38.fa.gz \
+    https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+
+wget \
+    -O large_files/hg38.chrom.sizes \
+    https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes
+
