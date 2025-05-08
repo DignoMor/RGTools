46b2003769b8fc009fba7298589fa01216dea80b
Xiuqi Pan <xp76@cornell.edu>
2025-05-08 14:56:50 -0400

    Add bed files to downlaoding list

--- download_large_files.sh
+++ download_large_files.sh
@@ -1,4 +1,5 @@
 
+# K562 GROcap tracks
 wget \
     -O large_files/ENCFF775FNU.mn.bw \
     https://www.encodeproject.org/files/ENCFF775FNU/@@download/ENCFF775FNU.bigWig
@@ -7,6 +8,13 @@ wget \
     -O large_files/ENCFF565BWR.pl.bw \
     https://www.encodeproject.org/files/ENCFF565BWR/@@download/ENCFF565BWR.bigWig
 
+# K562 GROcap peaks
+wget \
+    -O large_files/ENCFF156JSS.bedTRE.gz \
+    https://www.encodeproject.org/files/ENCFF156JSS/@@download/ENCFF156JSS.bed.gz
+gunzip large_files/ENCFF156JSS.bedTRE.gz
+
+# hg38 reference genome
 wget \
     -O large_files/hg38.fa.gz \
     https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
