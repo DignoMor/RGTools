efab5b081e59b67018d2c63e5dfdfc098a95e736
Xiuqi Pan <xp76@cornell.edu>
2025-11-23 17:42:28 -0500

    Create ListFile class

---
+++ ListFile.py
@@ -0,0 +1,61 @@
+
+import sys
+
+import numpy as np
+
+class ListFile:
+    '''
+    Class for reading and handling list files.
+    A list file is a simple text file with one item per line.
+    '''
+    
+    def __init__(self, filter_empty_lines=True):
+        '''
+        Initialize a ListFile instance.
+
+        Keyword arguments:
+        - filter_empty_lines: If True, filter out empty lines when reading (default: True).
+        '''
+        self._contents = []
+        self._filter_empty_lines = filter_empty_lines
+
+    def read_file(self, file_path):
+        '''
+        Read a list file.
+
+        Keyword arguments:
+        - file_path: Path to the list file.
+                     If file_path is "-" or "stdin", read from stdin.
+        '''
+        if file_path == "-" or file_path == "stdin":
+            file_handle = sys.stdin
+        else:
+            file_handle = open(file_path, 'r')
+
+        lines = file_handle.readlines()
+        if self._filter_empty_lines:
+            self._contents = [line.strip() for line in lines if line.strip()]
+        else:
+            self._contents = [line.rstrip('\n\r') for line in lines]
+
+        if file_path != "-" and file_path != "stdin":
+            file_handle.close()
+
+    def get_contents(self):
+        '''
+        Return the contents of the list file as a list of strings.
+
+        Returns:
+        - contents: np.array of strings, one per line in the file.
+        '''
+        return np.array(self._contents)
+
+    def get_num_lines(self):
+        '''
+        Return the number of lines in the list file.
+
+        Returns:
+        - num_lines: Number of lines.
+        '''
+        return len(self._contents)
+
